f85f7ceaee63085e251284bb0712e1ca2fa6b2b3
Jamie Yang <yangjm1287@gmail.com>
2023-10-31 14:52:59 -0400

    include api in fetch link

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://courageous-capris-eel.cyclic.app/${rapperName}`)
+        const response = await fetch(`https://courageous-capris-eel.cyclic.app/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
